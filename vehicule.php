af4a1e6f50956d399829ed759caac9464cb34e8a
Antoine <antoine.lefebvre@edu.itescia.fr>
2018-12-10 14:12:21 +0100

    class vehicule

--- vehicule.php
+++ vehicule.php
@@ -2,23 +2,34 @@
 
 
 class vehicule{
- 
-    private function(){
+
+    private $_size_x;
+    private $_size_y;
+    private $_size_z;
+    private $_vitesse;
+    private $_energie;
+    private $_poids;
+
+
+    public function demarrer(){
         
     }
+    public function arreter(){
 
+    }
 
-    
+    public function accelerer(){
+
+    }
+
+    public function freiner(){
+
+    }
+
+    public function tourner(){
+
+    }
 }
-
-
-
-
-
-
-
-
-
 
 
 
