94b6080439e332af6dd6944196ab8ed8b27578f3
Unknown <anthony.guiock@edu.itescia.fr>
2018-12-11 15:27:15 +0100

    Correction Eau

--- class/eau.php
+++ class/eau.php
@@ -1,29 +1,26 @@
 <?php
 
+require(vehicule.php);
 
-abstract class eau extends vehicule{
-
-
-protected $sonar;
-protected $radio;
+abstract class eau extends Vehicule{
 
 
-public function getSonar(){
-    return $this->sonar;
+	protected $sonar;
+	protected $radio;
+
+
+	public function getSonar(){
+ 	   return $this->sonar;
+	}
+	public function setSonar($son){
+	    $this->sonar = $son;
+	}
+
+	public function getRadio(){
+	    return $this->radio; 
+	}
+	public function setRadio($rad){
+    	$this->radio = $rad;
+	}
+
 }
-public function setSonar($son){
-    $this->sonar = $son;
-}
-
-public function getRadio(){
-    return $this->radio; 
-}
-public function setRadio($rad){
-    $this->radio = $rad;
-}
-
-}
-
-
-
-?>
