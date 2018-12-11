8edeb69ffdbafd662a2c7d4b2caf8b99742737b5
Antoine <antoine.lefebvre@edu.itescia.fr>
2018-12-11 16:13:03 +0100

    Voiture + Moto + Camion

--- class/camion.php
+++ class/camion.php
@@ -3,15 +3,36 @@
 class Camion extends Terre{
 
 	protected $wheel = 4;
+	protected $passager = 1;
 
 /** Attribut */
 
-	public function __construct($wheel){
-		$this->getwheel($wheel);
+	public function __construct($wheel) {
+		$this->wheel = $wheel;
+		$this->passager = $passager;
 	}
 
-	public function getwheel(){
-		echo "Ce véhicule terrestre à ".$wheel.".";
+	public function getwheel() {
+		echo "Ce véhicule terrestre à ".$wheel." roues.";
+	}
+
+	public function Setwheel($wheel) { /** si ton véhicule à moins de deux roues */
+		if ($wheel < 4){
+			$wheel + 1;
+		} else {
+			return $this->$wheel;
+		}
+	}
+
+	public function getpassager(){
+		return $this->passager;
+	}
+	public function setPassager($passager){
+		if($passager > 4){
+			echo "Il n'y a plus de place dans le vehicule";
+		} else {
+			echo "Il y a de place dans le vehicule";
+		} 
 	}
 
 /** Méthode */
--- class/moto.php
+++ class/moto.php
@@ -3,15 +3,37 @@
 class Moto extends Terre{
 
 	protected $wheel = 4;
+	protected $passager = 1;
 
 /** Attribut */
 
-	public function __construct($wheel){
-		$this->getwheel($wheel);
+	public function __construct($wheel, $passager) {
+		$this->wheel = $wheel;
+		$this->passager = $passager;
 	}
 
-	public function getwheel(){
-		echo "Ce véhicule terrestre à ".$wheel.".";
+
+	public function getwheel() {
+		echo "Ce véhicule terrestre à ".$wheel." roues.";
+	}
+
+	public function Setwheel($wheel) { /** si ton véhicule à moins de deux roues */
+		if ($wheel < 2 && $wheel > 3){
+			$wheel + 1;
+		} else {
+			return $this->$wheel;
+		}
+	}
+
+	public function getpassager(){
+		return $this->passager;
+	}
+	public function setPassager($passager){
+		if($passager > 2){
+			echo "Il n'y a plus de place dans le vehicule";
+		} else {
+			echo "Il y a de place dans le vehicule";
+		} 
 	}
 
 /** Méthode */
