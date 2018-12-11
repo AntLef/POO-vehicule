02dac3e9b35e147a798ed95ef8306cea9b3acb82
Kalvin <kalvin.mbassi@edu.itescia.fr>
2018-12-11 13:25:04 +0100

    Auto stash before merge of "master" and "origin/master"

--- class/eau.php
+++ class/eau.php
@@ -1,22 +1,53 @@
 <?php
 
 
-class vehicule{
+abstract class eau{
 
-    
+protected $moteur;
+protected $gouvernail;
+protected $coque;
+protected $sonar;
+protected $radio;
+
+
+public function getMoteur(){
+    return $this->moteur;
+}
+public function setMoteur($motor){
+    $this->moteur = $motor;
 }
 
 
+public function getGouvernail(){
+    return $this->gouvernail;
+}
+public function setGouvernail($rudder){
+    $this->gouvernail = $rudder;
+}
+   
 
+public function getCoque(){
+    return $this->coque;
+}
+public function setCoque ($shell){
+    $this->coque = $shell;
+}
 
+public function getSonar(){
+    return $this->sonar;
+}
+public function setSonar($son){
+    $this->sonar = $son;
+}
 
+public function getRadio(){
+    return $this->radio; 
+}
+public function setRadio($rad){
+    $this->radio = $rad;
+}
 
-
-
-
-
-
-
+}
 
 
 
